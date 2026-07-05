162ddc62f0c4b195cccd27cd394712e232696ea6
gabriellopes754 <gabriellopes15942@outlook.com>
2026-07-05 14:43:42 -0300

    Cria entidade Task

---
+++ src/main/java/com/gabriel/gerenciadortarefas/model/Task.java
@@ -0,0 +1,64 @@
+package com.gabriel.gerenciadortarefas.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+
+public class Task {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        private String titulo;
+
+        private String descricao;
+
+        private String status;
+
+        private String prioridade;
+
+        public Task() {
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getTitulo() {
+            return titulo;
+        }
+
+        public void setTitulo(String titulo) {
+            this.titulo = titulo;
+        }
+
+        public String getDescricao() {
+            return descricao;
+        }
+
+        public void setDescricao(String descricao) {
+            this.descricao = descricao;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public String getPrioridade() {
+            return prioridade;
+        }
+
+        public void setPrioridade(String prioridade) {
+            this.prioridade = prioridade;
+        }
+    }
+
